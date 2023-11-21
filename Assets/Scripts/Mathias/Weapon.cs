7cc60c723e1102ffb01b393827d6b702adb341c4
Mathias Kofoed Hansen <mkha23@student.aau.dk>
2023-11-21 14:45:27 +0100

    Burst og shooting virker

--- Assets/Scripts/Mathias/Weapon.cs
+++ Assets/Scripts/Mathias/Weapon.cs
@@ -4,33 +4,106 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    
     public GameObject bulletPrefab;
     public Transform firePoint;
+    public float bulletSpeed = 10f;
+    public float timeBetweenShots = 0.2f;
+    public int burstShots = 3;
+    public float burstCooldown = 3f;
 
-    // Update is called once per frame
+    private bool canShoot = true;
+    private int shotsRemaining;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        // Check for arrow key input and start burst immediately
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) ||
+            Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Shoot(Vector2.up);
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Shoot(Vector2.right);
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            Shoot(Vector2.down);
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            Shoot(Vector2.left);
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                StartCoroutine(ShootBurst(Vector2.up));
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                StartCoroutine(ShootBurst(Vector2.right));
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                StartCoroutine(ShootBurst(Vector2.down));
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                StartCoroutine(ShootBurst(Vector2.left));
+            }
         }
     }
 
-    void Shoot(Vector2 direction)
+    IEnumerator ShootBurst(Vector2 direction)
     {
-       GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        if (canShoot)
+        {
+            canShoot = false;
+
+            for (int i = 0; i < burstShots; i++)
+            {
+                ShootBullet(direction);
+                yield return new WaitForSeconds(timeBetweenShots);
+            }
+
+            yield return new WaitForSeconds(burstCooldown);
+
+            canShoot = true;
+        }
     }
 
+    void ShootBullet(Vector2 shotDirection)
+    {
+        // Instantiate bullet and shoot in the determined direction
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+        bulletRb.AddForce(shotDirection * bulletSpeed, ForceMode2D.Impulse);
+    }
 }
+
+
+    /* void Update()
+     {
+
+         //Depending on which arrow key the player presses, the Shoot() parameter is changed to that direction
+         if (Input.GetKeyDown(KeyCode.UpArrow))
+         {
+             Shoot(Vector2.up);
+         }
+         else if (Input.GetKeyDown(KeyCode.RightArrow))
+         {
+             Shoot(Vector2.right);
+         }
+         else if (Input.GetKeyDown(KeyCode.DownArrow))
+         {
+             Shoot(Vector2.down);
+         }
+         else if (Input.GetKeyDown(KeyCode.LeftArrow))
+         {
+             Shoot(Vector2.left);
+         }
+     }
+
+     void Shoot(Vector2 direction)
+     {
+         //The bullet is instantiated into the game as the bullet prefab at the firePoint position. Quaternion.identity ensures that the bullet has no rotation
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+
+
+         Bullet bulletScript = bullet.GetComponent<Bullet>();
+
+         Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+         if (bulletRb != null)
+         {
+             bulletRb.AddForce(direction * bulletScript.bulletSpeed, ForceMode2D.Impulse);
+             Debug.Log("Direction:" + direction);
+         }
+     }*/
+
+
