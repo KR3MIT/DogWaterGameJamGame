00351f2eccc1211ce24c31efb11c4a6d5f27372b
Oskar B. Kaare <oskar.kaare@gmail.com>
2023-11-21 13:31:42 +0100

    dasdas

--- Assets/Scripts/Oskar/PlayerBehavior.cs
+++ Assets/Scripts/Oskar/PlayerBehavior.cs
@@ -3,34 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerBehaviour : MonoBehaviour
 {
-    private Vector2 movement;
-    private Rigidbody2D myBody;
-    public int speed = 5;
+    public float speed;
     public float jump;
+    private float Move;
+
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        myBody = GetComponent<Rigidbody2D>();
-        
-    }
-    private void OnMovement(InputValue value)
-    {
-        movement = value.Get<Vector2>();
+     
+
+
     }
 
-    private void FixedUpdate()
-    {
-        myBody.velocity = movement * speed;
-    }
     void Update()
     {
+        Move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+
+
         if (Input.GetButtonDown("Jump"))
         {
-            myBody.AddForce(new Vector2(myBody.velocity.x, jump));
-            Debug.LogFormat("hop");
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
         }
-      
     }
 }
