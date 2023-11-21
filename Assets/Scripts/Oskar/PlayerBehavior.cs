c3b7a9b651923610fabeb4d1a442ea10dc6d1e2e
Oskar B. Kaare <oskar.kaare@gmail.com>
2023-11-21 12:50:09 +0100

    assets

--- Assets/Scripts/Oskar/PlayerBehavior.cs
+++ Assets/Scripts/Oskar/PlayerBehavior.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerBehaviour : MonoBehaviour
 {
+    private Vector2 movement;
+    private Rigidbody2D myBody;
+    public int speed = 5;
+    public float jump;
     // Start is called before the first frame update
     void Start()
     {
+        myBody = GetComponent<Rigidbody2D>();
         
     }
+    private void OnMovement(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        myBody.velocity = movement * speed;
+    }
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            myBody.AddForce(new Vector2(myBody.velocity.x, jump));
+            Debug.LogFormat("hop");
+        }
+      
     }
 }
