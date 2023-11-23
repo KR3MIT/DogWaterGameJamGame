e26b311c1f4e884b8c925ca1bcba3f71269167c8
KR3MIT <jason.cm.hassan@gmail.com>
2023-11-23 09:57:00 +0100

    goomba update hot fix

--- Assets/Enemy/Goomba/Goomba.cs
+++ Assets/Enemy/Goomba/Goomba.cs
@@ -53,7 +53,7 @@ public class Goomba : MonoBehaviour
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "bullet")
+        if (collision.gameObject.tag == "Bullet")
         {
             walkDirection = 0;
             Destroy(gameObject);
