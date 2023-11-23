15c4c02620b241de379f45bbda76f7dd18ba92d1
Oskar B. Kaare <oskar.kaare@gmail.com>
2023-11-23 10:26:34 +0100

    win screen and return button

--- Assets/Scripts/Oskar/ChangeScreneBehavior.cs
+++ Assets/Scripts/Oskar/ChangeScreneBehavior.cs
@@ -5,12 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScreneBehavior : MonoBehaviour
 {
-  public void OnTriggerEnter2D(Collider2D other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Exit"))
         {
             SceneManager.LoadScene(2);
             Debug.LogFormat("load");
         }
+    }
+    public void LoadScene(string sceneid)
+    {
+        SceneManager.LoadScene(sceneid);
+        Debug.LogFormat("load2");
     }
 }
