6b8a8a51b3b761a9a88912e7e591014729aa3dcb
Oskar B. Kaare <oskar.kaare@gmail.com>
2023-11-23 10:30:33 +0100

    Update ChangeScreneBehavior.cs

--- Assets/Scripts/Oskar/ChangeScreneBehavior.cs
+++ Assets/Scripts/Oskar/ChangeScreneBehavior.cs
@@ -5,17 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScreneBehavior : MonoBehaviour
 {
+    //if something collides with the collider of the holder of the script
     public void OnTriggerEnter2D(Collider2D other)
     {
+        //check if the collider/object has the tag "Exit"
         if (other.gameObject.CompareTag("Exit"))
         {
+            //if it does, load scene 2
             SceneManager.LoadScene(2);
+            //and print load in console
             Debug.LogFormat("load");
         }
     }
-    public void LoadScene(string sceneid)
+    //this is set to onlick on the button so it Loads the scene of the string name 
+    public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneid);
+        //when button is pressed load scene with the name set in unity
+        SceneManager.LoadScene(sceneName);
+        //aand print load 2
         Debug.LogFormat("load2");
     }
 }
